ddfc6ff9b666ea17ba33cdbe880b3ea01f87c10f
Paul Susmarski <paul.susmarski@nrel.gov>
2021-10-26 15:44:36 -0600

    Undo bad change

--- src/components/menu/menu-link.tsx
+++ src/components/menu/menu-link.tsx
@@ -52,7 +52,7 @@ function MenuLink({
 
   return (
     <li className={`menu-item ${isCurrentClass} ${className}`} onClick={handleClick}>
-      <span>{children} NOTE</span>
+      <span>{children}</span>
     </li>
   );
 }
